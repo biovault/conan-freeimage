3fc7799ffeff0430b294ef17ff1330de4ba0ef50
Baldur van Lew <baldurvanlew@gmail.com>
2019-09-16 14:37:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## A conan packager for FreeImage
+
+Package the [FreeImage](http://freeimage.sourceforge.net/download.html) utility using conan for Macos, Linux and Windows.
+
+Parts of the conan file are derived from the excellent [package example by Michaël Roynard](https://gitlab.lrde.epita.fr/olena/conan-freeimage). This version diverges by simply taking the prebuilt version for Windows.
+
+FreeImage retains it's original license but this repo is under the MIT license.
---
+++ conanfile.py
@@ -0,0 +1,56 @@
+import os
+from conans import ConanFile
+from conans import tools
+from conans import AutoToolsBuildEnvironment
+
+
+class FreeImageConan(ConanFile):
+    name = "freeimage"
+    version = "3.18"
+    description = "FreeImage for Windows, pre-built binaries are supplied"
+    license = "FIPL"
+    settings = "os_build", "compiler", "build_type", "arch_build"
+    url = "https://github.com/bldrvnlw/conan-freeimage"
+
+    def configure(self):
+        if self.settings.os_build != "Windows":
+            raise Exception("Only Windows is supported by this recipe")
+
+    def source(self): 
+        if self.settings.os_build != "Windows":
+            tools.get("http://downloads.sourceforge.net/freeimage/FreeImage3180.zip")
+            
+            
+    def build(self):
+        if self.settings.os_build == "Windows":
+            tools.get("http://downloads.sourceforge.net/freeimage/FreeImage3180Win32Win64.zip"
+        else:
+            autotools = AutoToolsBuildEnvironment(self)
+            env_build_vars = autotools.vars
+            env_build_vars['DESTDIR'] = self.package_folder
+            autotools.make(vars=env_build_vars)
+        
+    def package(self):
+        if self.settings.os_build = "Windows":
+            if self.settings.arch_build == "x86_64":
+                src = os.path.join(self.build_folder, "FreeImage/Dist/x64")
+            elif self.settings.arch_build == "x86":
+                src = os.path.join(self.build_folder, "FreeImage/Dist/x32")
+                
+            self.copy("*.lib", dst="lib", src=src, keep_path=False)
+            self.copy("*.dll", dst="bin", src=src, keep_path=False)
+            self.copy("*.h", dst="include", src=src, keep_path=False)
+        else:
+            autotools = AutoToolsBuildEnvironment(self)
+            env_build_vars = autotools.vars
+            env_build_vars['DESTDIR'] = self.package_folder
+            autotools.install(vars=env_build_vars)
+
+            with tools.environment_append(autotools.vars):
+                self.run(
+                    "mv {}/usr/* {}/".format(self.package_folder, self.package_folder))
+                self.run("rm -rf {}/usr".format(self.package_folder))
+        
+
+    def package_info(self):
+        self.cpp_info.libs = ["freeimage"]
