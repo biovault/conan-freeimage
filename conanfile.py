49b8e77050344072a042c49cecf7f7e6909ea3a3
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-08 09:21:41 +0200

    Move disable perf to macos

--- conanfile.py
+++ conanfile.py
@@ -35,9 +35,9 @@ class FreeImageConan(ConanFile):
                 # FIP : Makefile.fip is for FreeImagePlus, the C++ FreeImage wrapper
                 # make 
                 if self.settings.os_build == "Macos":
+                    autotools.defines.append("DISABLE_PERF_MEASUREMENT=1")
                     autotools.make(target="-f Makefile.osx", vars=env_build_vars)
                 else:
-                    autotools.defines.append("DISABLE_PERF_MEASUREMENT=1")
                     autotools.make(target="-f Makefile.gnu", vars=env_build_vars)
                 print("Cur dir: ", os.getcwd(), " Dist subdir: ", os.listdir("./Dist"))
                 self._dist_subfolder = os.path.join(os.getcwd(), "Dist");
